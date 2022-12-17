41de66035ad4a803f77e872393491ccad145c07e
woowenjun99 <woowenjun99@gmail.com>
2022-12-18 00:45:52 +0800

    Add in the layout for the home page

---
+++ layouts/home.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import clsx from "clsx";
+</script>
+
+<template>
+  <main
+    :class="
+      clsx(
+        'flex min-h-screen',
+        'flex-col items-center',
+        'justify-center bg-gradient-to-b',
+        'from-[#FFE2E2] to-[#FFC7C7]'
+      )
+    "
+  >
+    <slot />
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+// Use the custom home layout for the application
+definePageMeta({
+  layout: "home",
+});
+</script>
+
+<template>
+</template>
