fd86e61ccec9d71e02c9784ad8336d0ca0071895
woowenjun99 <woowenjun99@gmail.com>
2022-12-21 22:14:43 +0800

    Fix the bug where I will be redirected twice

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -3,6 +3,9 @@ import GoogleProvider from "next-auth/providers/google";
 
 export default NuxtAuthHandler({
   secret: process.env.NUXT_SECRET,
+  pages: {
+    signIn: "/auth/login",
+  },
   providers: [
     // @ts-ignore Import is exported on .default during SSR, so we need to call it this way. May be fixed via Vite at some point
     GoogleProvider.default({
